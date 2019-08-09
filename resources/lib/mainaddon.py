8f1a39623755ec93a285b7671ff4dbaf39d24458
leopheard <leopheard.misc@gmail.com>
2019-08-09 13:23:26 -0400

    upload

--- resources/lib/mainaddon.py
+++ resources/lib/mainaddon.py
@@ -32,8 +32,8 @@ def get_playable_podcast(soup):
 #            desc = content.find('description')
 #            desc = desc.get_text()
 
-            thumbnail = content.find('itunes:image')
-            thumbnail = desc.get_text('href')
+#            thumbnail = content.find('itunes:image')
+#            thumbnail = thumbnail.get_text('href')
 
         except AttributeError:
             continue
@@ -42,7 +42,7 @@ def get_playable_podcast(soup):
                 'url': link,
                 'title': title,
 #                'desc': desc,
-                'thumbnail': thumbnail,
+                'thumbnail': "https://cdn.changelog.com/uploads/covers/brain-science-medium.png?v=63725770749",
         }
         
         subjects.append(item) 
@@ -84,8 +84,8 @@ def get_playable_podcast1(soup):
 #            desc = content.find('description')
 #            desc = desc.get_text()
 
-            thumbnail = content.find('itunes:image')
-            thumbnail = desc.get_text('href')
+#            thumbnail = content.find('itunes:image')
+#            thumbnail = thumbnail.get_text('href')
 
         except AttributeError:
             continue
@@ -94,7 +94,7 @@ def get_playable_podcast1(soup):
                 'url': link,
                 'title': title,
 #                'desc': desc,
-                'thumbnail': thumbnail,
+                'thumbnail': "https://cdn.changelog.com/uploads/covers/brain-science-medium.png?v=63725770749",
         }
         
         subjects.append(item) 
